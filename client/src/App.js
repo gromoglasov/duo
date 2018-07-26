7adeda887b0fc929f1f7a62b701f69116a6fd175
Volodymyr Gromoglasov <gromoglasov96@gmail.com>
2018-07-26 16:14:19 +0200

    refactored the code to use Tone.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,75 +11,37 @@ class App extends Component {
 
 
   constructor (props) {
-    let synth = new Tone.Synth().toMaster();
-    console.log(synth);
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4', '8n');
-
     super(props);
-    // create web audio api context
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioCtx = new AudioContext();
+    const synth = new Tone.PolySynth(108, Tone.AMSynth).toMaster();
     const allNotes = allKeys;
     this.state = {
-      audioCtx,
+      synth,
       allNotes,
     };
-    const allOscilators = this.renderOscilators()[0];
-    const allGainNodes = this.renderOscilators()[1];
-    this.state = {
-      audioCtx,
-      allNotes,
-      allOscilators,
-      allGainNodes
-    };
-  }
-
-  renderOscilators () {
-    let arr = [];
-    let oscillatorsObj = {};
-    let gainNodeObj = {};
-    for (let i=0; i < allKeys.length; i++) {
-      for (let k=0; k < allKeys[i].length; k++) {
-        let oscillator = this.state.audioCtx.createOscillator();
-        let gainNode = this.state.audioCtx.createGain();
-        oscillator.connect(gainNode);
-
-        oscillator.detune.value = 50;
-        oscillator.frequency.value = allKeys[i][k].freq;
-        // oscillator.type = makeDistortionCurve(50);
-        oscillatorsObj[allKeys[i][k].freq] = oscillator;
-        gainNodeObj[allKeys[i][k].freq] = gainNode;
-        oscillator.start();
-      }
-    }
-    arr.push(oscillatorsObj);
-    arr.push(gainNodeObj);
-    return arr;
   }
 
   toggleSound (note) {
 
-    console.log(note);
-    console.log(this.state.allOscilators);
-    console.log(this.state.allOscilators[note]);
+    // console.log(note);
+    // console.log(this.state.allOscilators);
+    // console.log(this.state.allOscilators[note]);
 
     let index = this.props.activeKeys.indexOf(note);
     if (index < 0) {
       this.props.playNote(note);
-      this.state.allGainNodes[note].connect(this.state.audioCtx.destination);
+      this.state.synth.triggerAttack(note);
+
     } else {
       this.props.stopNote(note);
-      this.state.allGainNodes[note].disconnect(this.state.audioCtx.destination);
+      this.state.synth.triggerRelease(note);
+
     }
 
   }
 
   render () {
     console.log(this.props.activeKeys);
-    console.log(this.state.activeOscilators);
     console.log(this.state);
-    // this.play();
     return (
       <div className="App">
         <Header />
